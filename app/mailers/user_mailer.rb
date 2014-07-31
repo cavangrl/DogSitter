a4abe076230238d6b4ed0af36dc02be2d8eae46a
cavangrl <pae.oconnor@gmail.com>
2014-07-31 18:22:48 -0400

    quick change

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,11 @@ class UserMailer < ActionMailer::Base
 
   default from: "pae.oconnor@gmail.com"
 
+   def registration_confirmation(id)
+    @user = User.find(id)
+    mail to: @user.email, subject: "Welcome to our site"
+  end
+
   def password_changed(id)
     @user = User.find(id)
     mail to: @user.email, subject: "Your password has changed"
